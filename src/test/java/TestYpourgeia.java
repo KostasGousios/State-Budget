9fe6fc7968e70ca6acb8fa3a8f84320520817d56
Nikos Chaidogiannis <nchaidogiannis@gmail.com>
2025-11-26 13:28:57 +0000

    δημιουργια test για υπουργεια

---
+++ src/test/java/TestYpourgeia.java
@@ -0,0 +1,6 @@
+public class TestYpourgeia {
+    public static void main(String[] args) {
+        Ypourgeia yp = new YpourgeioPaideias(); //κληση κατασκευαστη και εισαγωγη ποσων απο υπουργειο
+        yp.katanomiProypApoProthypoyrgo(); //πρωθυπουργος δινει ποσο
+    }
+}
