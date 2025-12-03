34876a119e55f9e6aac4d7fe73c45044f47a795e
StefanosPapachristos <stefpapcps@gmail.com>
2025-12-03 13:54:20 +0200

    allaxa onoma mias metavlhths

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,44 @@ public class Main {
     public static void main(String[] args) {
                Scanner input = new Scanner(System.in); //Δημιουργία αντικειμένου για εισαγωγή αριθμών
         
-        // Αρχικό ποσό στο ταμείο
+        // ΔΙΑΧΕΙΡΗΣΗ ΜΕΝΟΥ ΕΠΙΛΟΓΩΝ enum 
+        System.out.println("διαλεξε μια απο τις παρακατω επιλογες");
+
+        for (MenuOptions option : MenuOptions.values()) {
+             System.out.println((option.ordinal() + 1) + "." + option.getDescription());
+        }
+        int  choice = input.nextInt();
+
+        MenuOptions selectedOption = MenuOptions.values()[choice - 1];
+        System.out.println("επελεξες" + selectedOption.getDescription());
+
+        // ΔΙΑΧΕΙΡΙΣΗ ΜΕΝΟΥ ΕΠΙΛΟΓΩΝ ΓΙΑ ΥΠΟΥΡΓΕΙΟ Παιδειας 
+        
+        if (choice == 2) {
+             System.out.println("διαλεξε μια απο  τις παρακατω επιλογες");
+
+            for (MenouOPtionsforYpPaideias option : MenouOPtionsforYpPaideias.values()) {
+                 System.out.println((option.ordinal() + 1) + "." + option.getDescriptionYgeias());
+            }
+            int choice2 = input.nextInt();
+
+            MenouOPtionsforYpPaideias selectedOpt = MenouOPtionsforYpPaideias.values()[choice2-1];
+            System.out.println("επελεξες" + "." + selectedOpt.getDescriptionPaideias());
+
+        // ΔΙΑΧΕΙΡΙΣΗ ΜΕΝΟΥ ΕΠΙΛΟΓΩΝ ΓΙΑ ΥΠΟΥΡΓΕΙΟ ΥΓΕΙΑΣ 
+        if (choice == 3) {
+             System.out.println("διαλεξε μια απο  τις παρακατω επιλογες");
+
+            for (MenouOPtionsforYpYgeias option : MenouOPtionsforYpYgeias.values()) {
+                 System.out.println((option.ordinal() + 1) + "." + option.getDescriptionYgeias());
+            }
+            int choice3 = input.nextInt();
+
+            MenouOPtionsforYpYgeias selectedOptygeias  = MenouOPtionsforYpYgeias.values()[choice2-1];
+            System.out.println("επελεξες" + "." + selectedOptygeias.getDescriptionYgeias()); }
+            
+     // Αρχικό ποσό στο ταμείο
+        
         System.out.print("Δώσε το αρχικό ποσό του κρατικού ταμείου από το προηγούμενο έτος: ");
         int arxikoPoso = input.nextInt();
         KratikoTameio tameio = new KratikoTameio(arxikoPoso);
@@ -39,44 +76,6 @@ public class Main {
         System.out.println("Νέο ποσό στο κρατικό ταμείο: " + tameio.getTameio());
 
         input.close();
-    
-    // ΔΙΑΧΕΙΡΗΣΗ ΜΕΝΟΥ ΕΠΙΛΟΓΩΝ enum 
-        System.out.println("διαλεξε μια απο τις παρακατω επιλογες");
-
-        for (MenuOptions option : MenuOptions.values()) {
-             System.out.println((option.ordinal() + 1) + "." + option.getDescription());
-        }
-        int choice = input.nextInt();
-
-        MenuOptions selectedOption = MenuOptions.values()[choice - 1];
-        System.out.println("επελεξες" + selectedOption.getDescription());
-    
-    // ΔΙΑΧΕΙΡΙΣΗ ΜΕΝΟΥ ΕΠΙΛΟΓΩΝ ΓΙΑ ΥΠΥΡΓΕΙΟ ΠΑΙΔΕΙΑΣ 
-        if (choice == 2) {
-            System.out.println("διαλεξε μια απο  τις παρακατω επιλογες");
-
-            for (MenouOptionsforYpPaideias option : MenouOptionsforYpPaideias.values()) {
-                 System.out.println((option.ordinal() + 1) + "." + option.getDescriptionPaideia());
-            }
-            int choice2 = input.nextInt();
-
-            MenouOptionsforYpPaideias selectedOpt = MenouOptionsforYpPaideias.values()[choice2-1];
-            System.out.println("επελεξες" + "." + selectedOpt.getDescriptionPaideia());
-        }
-    // ΔΙΑΧΕΙΡΙΣΗ ΜΕΝΟΥ ΕΠΙΛΟΓΩΝ ΓΙΑ ΥΠΟΥΡΓΕΙΟ ΥΓΕΙΑΣ 
-        if (choice == 3) {
-             System.out.println("διαλεξε μια απο  τις παρακατω επιλογες");
-
-            for (MenouOPtionsforYpYgeias option : MenouOPtionsforYpYgeias.values()) {
-                 System.out.println((option.ordinal() + 1) + "." + option.getDescriptionYgeias());
-            }
-            int choice2 = input.nextInt();
-
-            MenouOPtionsforYpYgeias selectedOpt = MenouOPtionsforYpYgeias.values()[choice2-1];
-            System.out.println("επελεξες" + "." + selectedOpt.getDescriptionYgeias());
-            
-
-
-        }
     }
+}
 }
